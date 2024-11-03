37391f917c88216d78192da7e33696ebfb4b402d
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-11-03 10:17:13 -0800

    Add files via upload

---
+++ whileLoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int count=0;
+	int temp=50;
+	while(count<50)
+	{
+		cout<<temp<<" I have to succed."<<endl;
+		count=count+1;
+		temp=temp-1;
+	}
+
+	
+}
